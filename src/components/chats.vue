80b0a0d5b89a678d589c8eb92dd0b5e7ec2c1f6e
gustavorteuber <gugareis135@gmail.com>
2023-01-06 18:47:49 -0300

    chats-pegando

---
+++ src/components/chats.vue
@@ -0,0 +1,84 @@
+<script>
+import { mapState } from "pinia";
+import { useAuthStore } from "@/stores/auth";
+export default {
+  props: ["comentarios"],
+  data() {
+    return {
+      superuser: "",
+      value: 1,
+      user: {},
+    };
+  },
+  computed: {
+    ...mapState(useAuthStore, ["is_superuser"]),
+  },
+  
+};
+</script>
+
+<template>
+  <div class="full">
+    <div class="comentarios">
+      <RouterLink to="/singout"
+        ><h1>{{ comentarios.autor.username }}</h1></RouterLink
+      >
+      <div class="fuso">
+        <h2>{{ comentarios.texto }}</h2>
+        <a>{{ comentarios.data }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.leaflet-popup-content-wrapper,
+.leaflet-popup-tip {
+  background-color: #fff0ac;
+}
+.remove {
+  display: flex;
+  align-items: center;
+}
+.fuso a {
+  font-size: 10pt;
+  font-weight: 300pt;
+  background: #eef;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.comentarios {
+  max-height: 500px;
+  max-width: 750px;
+  border-radius: 0px 10px 10px 10px;
+  border: 1px solid #eef;
+  background-color: #a3944f;
+  display: flex;
+  margin-top: 20px;
+  margin-left: 20px;
+}
+h1 {
+  background: -webkit-linear-gradient(45deg,
+    #d3c6cc,
+    #e2c3c6,
+    #eecfc4,
+    #f8e6c6,
+    #ffffcc);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 16pt;
+}
+h2 {
+  word-break: break-all;
+  margin-top: 7px;
+  margin-left: 10px;
+  font-size: 10pt;
+}
+.full {
+  max-width: 500px;
+  margin-left: 5%;
+  display: flex;
+  color: #eff;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,90 @@
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+import chats from "@/components/chats.vue"
+import axios from "axios";
+import { mapState } from "pinia";
+import { useAuthStore } from "@/stores/auth";
+export default {
+  data() {
+    return {
+      value: 1,
+      user: {},
+      superuser: "",
+      comentarios: [],
+      comentario: {
+        texto: "",
+        autor: 0,
+      },
+    };
+  },
+  components: { Sidebar, chats },
+  computed: {
+    ...mapState(useAuthStore, ["id", "is_superuser", "username"]),
+  },
+  methods: {
+    async addComment() {
+      if (this.comentario.texto.trim() === "") {
+        return;
+      }
+      this.comentario.autor = this.id;
+      await axios.post(
+        "http://localhost:8000/chats/",
+        this.comentario
+      );
+      await this.getAllComments();
+    },
+    async getAllComments() {
+      const comentarios = await axios.get(
+        "http://localhost:8000/chats/"
+      );
+      this.comentarios = comentarios.data;
+    },
+  },
+  async created() {
+    await this.getAllComments();
+  },
+  
+};
+</script>
 <template>
   <div class="all">
     <Sidebar />
+    
     <main class="home-page">
-      <h1>home</h1>
-      <p>This is the home page</p>
+      <div class="head">
+        <i class="fa-solid fa-user"></i>
+        <span>{{ username }}</span>
+      </div>
+      <div class="mensages"></div>
+      <chats
+        v-for="comentario in comentarios"
+        :key="comentario.id"
+        :comentarios="comentario"
+      />
+      <div class="send-mensage">
+
+        <div class="submit">
+          <input
+            @keydown.enter="addComment()"
+            type="text"
+            style="padding: 4px"
+            placeholder="escreva seu comentario
+              "
+            v-model="comentario.texto"
+          />
+          <button
+            v-on:click.prevent="addComment"
+            type="submit"
+            class="btn btn-primary"
+          >
+            Enviar
+          </button>
+        <i class="fa-solid fa-paperclip"></i>
+        </div>
+      </div>
     </main>
   </div>
 </template>
-<script>
-import Sidebar from "@/components/Sidebar.vue";
-export default {
-  components: { Sidebar },
-};
-</script>
 <style scoped>
 .all {
   display: flex;
@@ -20,5 +92,60 @@ export default {
 
 .home-page {
   width: 100vw;
+}
+.head {
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  height: 13vh;
+  background: linear-gradient(
+    45deg,
+    #fffccc,
+    #ffffcc,
+    #eecfc4,
+    #d3c6cc,
+    #e2c3c6
+  );
+}
+.head span {
+  margin-left: 1vw;
+  font-family: "Poppins", sans-serif;
+  font-size: 15pt;
+  font-weight: 600;
+}
+.head i {
+  margin-left: 5vw;
+  font-size: 20pt;
+}
+.mensages {
+  height: 77vh;
+}
+.send-mensage {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  height: 10vh;
+  background: linear-gradient(
+    45deg,
+    #f0d1d4,
+    #e2c3c6,
+    #eecfc4,
+    #fffccc,
+    #ffffcc
+  );
+}
+.input-mensage {
+  height: 50%;
+  width: 70%;
+  margin-left: 2vw;
+  border: #3a3737 1px solid;
+  border-radius: 15px;
+}
+input::placeholder {
+  padding: 1.5vw;
+}
+.send-mensage i {
+  font-size: 20pt;
+  margin-left: 5vw;
 }
 </style>
