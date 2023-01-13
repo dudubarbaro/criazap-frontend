34c3047516f4e6eec92dcd8e5a8d4d8b144be9e2
gustavorteuber <gugareis135@gmail.com>
2023-01-13 17:33:10 -0300

    alteração-dash

--- src/views/master/dashboard.vue
+++ src/views/master/dashboard.vue
@@ -234,7 +234,7 @@ export default {
                   >Sobre nós</a
                 >
                 <a
-                  href="/login"
+                  href="/"
                   class="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
                   tabindex="-1"
